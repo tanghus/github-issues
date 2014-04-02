7eef808cbfd1b5cb235a74d0911186c9b416ad90
Thomas Tanghus <thomas@tanghus.net>
2014-04-02 17:03:26 +0200

    Update title on context change

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@
 		function($routeProvider) {
 			$routeProvider.
 			when('/:org/:repo', {
+				title: 'Repo',
 				templateUrl: 'templates/issues.php',
 				controller: 'IssuesCtrl'
 			}).
@@ -25,17 +26,39 @@
 				redirectTo: '/:org/:repo/:issue'
 			}).*/
 			when('/:org/:repo/:issue', {
+				title: 'Issue',
 				templateUrl: 'templates/issue.php',
 				controller: 'issueDetailCtrl'
 			}).
 			when('/:org/:repo/new', {
+				title: 'New',
 				templateUrl: 'templates/newissues.php',
 				controller: 'NewIssueCtrl'
 			}).
 			otherwise({
+				title: 'Intro',
 				templateUrl: 'templates/intro.php',
 				controller: 'IntroCtrl'
 				//redirectTo: '/'
+			});
+		}
+	])
+	.run(['$location', '$rootScope', '$document',
+		function($location, $rootScope, $document) {
+			// Update title base on context
+			$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+				if (!$rootScope.originalTitle) {
+					$rootScope.originalTitle = $document[0].title;
+				}
+				var title = '', context = [];
+				if (current.keys) {
+					angular.forEach(current.keys, function(key) {
+						context.push(current.params[key.name]);
+					});
+				}
+				var separator = context.length > 0 ? ' - ' : '';
+				title = 'Issues - ' + title + context.join('/') + separator + $rootScope.originalTitle;
+				$document[0].title = title;
 			});
 		}
 	]);
